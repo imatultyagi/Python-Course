b0d21675361a1944bef8ae576f9f3cb29a3b90c3
Atul Tyagi <140688207+imatultyagi@users.noreply.github.com>
2026-02-19 10:16:33 +0530

    Add shopping bill calculation script

---
+++ Selection Statement In Python/ShoppingBillCalculation.py
@@ -0,0 +1,23 @@
+# Q1. Shopping Bill Calculation
+
+
+# Prices and quantities
+notebooks = 3
+notebook_price = 45
+
+pens = 2
+pen_price = 20
+
+# Calculate total bill
+total_bill = (notebooks * notebook_price) + (pens * pen_price)
+
+# Amount given by customer
+amount_given = 500
+
+# Calculate remaining balance
+remaining_balance = amount_given - total_bill
+
+# Output
+print("Total Bill Amount: (Rs)", total_bill)
+print("Remaining Balance: (Rs)", remaining_balance)
+
