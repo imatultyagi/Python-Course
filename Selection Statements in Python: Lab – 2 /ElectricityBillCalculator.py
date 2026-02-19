d86b860dd83b1a47ceebcc08a91cf93948253a9b
Atul Tyagi <140688207+imatultyagi@users.noreply.github.com>
2026-02-19 14:40:12 +0530

    Add electricity bill calculator with senior citizen discount
    Implement an electricity bill calculator that calculates the bill based on units consumed and applies a discount for senior citizens.

---
+++ Selection Statements in Python: Lab – 2 /ElectricityBillCalculator.py
@@ -0,0 +1,26 @@
+# 1. Electricity Bill Calculator 
+# Calculate the electricity bill based on units consumed: 
+# • 0–100 → ₹5/unit 
+# • 101–300 → ₹7/unit 
+# • Above 300 → ₹10/unit 
+# If the user is a senior citizen, apply 10% discount.
+units = float(input("Enter the number of units consumed: "))
+#Units validation
+if units < 0:
+    print("Invalid number of units. Please enter a positive value.")
+    exit(0)
+is_senior_citizen = input("Are you a senior citizen? (yes/no): ").lower()
+#Senior citizen validation
+if is_senior_citizen not in ["yes", "no"]:
+    print("Invalid input for senior citizen. Please enter yes or no.")
+    exit(0)
+#Bill calculation
+if units <= 100:
+    bill = units * 5
+elif units > 100 and units <= 300:
+    bill = 100 * 5 + (units - 100) * 7
+else:
+    bill = 100 * 5 + 200 * 7 + (units - 300) * 10
+if is_senior_citizen == "yes":
+    bill *= 0.9
+print(f"The total electricity bill is: ₹{bill:.2f}")
