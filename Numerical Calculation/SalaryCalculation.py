69e2367c0d15919ae05f7604d0ae5971ab3c2eac
Atul Tyagi <140688207+imatultyagi@users.noreply.github.com>
2026-02-21 20:47:44 +0530

    Add salary calculation script

---
+++ Numerical Calculation/SalaryCalculation.py
@@ -0,0 +1,15 @@
+#Salary Calculation
+
+# Given values
+basic_salary = 25000
+bonus = 5000
+
+# Calculate HRA (20% of basic)
+hra = basic_salary * 20 / 100
+
+# Calculate total salary
+total_salary = basic_salary + hra + bonus
+
+# Output
+print("HRA: ₹", hra)
+print("Total Monthly Salary: ₹", total_salary)
